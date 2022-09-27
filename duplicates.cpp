93d80507778178185f48bd86e6d6c0cda327af95
Arvi-H <arvihaxhillari1@gmail.com>
2022-09-27 14:21:36 -0600

    Problem 1 - Finding Duplicates
    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.

---
+++ duplicates.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end()); 
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i != (nums.size()-1)) {
+                if (nums[i] == nums[i+1]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+};
+
+int main(int argc, char** argv) {
+    // ifstream in(argv[1]);
+    vector<int> nums {1,1,1,3,3,4,3,2,4,2};
+
+    Solution s;
+    if (s.containsDuplicate(nums)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    
+}   
